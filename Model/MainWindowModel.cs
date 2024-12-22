4ac9f2441e03d2f148a6e663decbe1f2bf43e52d
Leonardo Briley <leonardobriley09@gmail.com>
2024-12-22 11:19:45 -0700

    New Changes

--- Model/MainWindowModel.cs
+++ Model/MainWindowModel.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Documents;
 
 namespace Shit_Spotify.Models
 {
     public class MainWindowModel
     {
-        // hello
+        private ObservableCollection<ListItem> Songs { get; set; }
     }
 }
